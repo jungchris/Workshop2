0dfb60e6548c9b7461cb34d0c167a9327ebafb5e
Chris Jungmann <chris@incaffeine.com>
2020-02-21 10:42:54 -0700

    Initial Commit

---
+++ WorkshopSwiftUIBlog3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WorkshopSwiftUIBlog3
+//
+//  Created by Chris Jungmann on 2/21/20.
+//  Copyright © 2020 Chris Jungmann. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
